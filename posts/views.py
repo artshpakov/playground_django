99058ecc95d4b1f9aa9f65627cc5d05096f03037
artshpakov <artshpakov@yandex.ru>
2011-11-23 22:26:37 +0400

    A view refactoring

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
 from models import PostView
         
@@ -11,10 +10,7 @@ def posts_list(request):
     return render_to_response("posts/list.html", locals(), RequestContext(request))
 
 def posts_view(request, **kwargs):
-    try:
-        if set(('slug', 'language', 'year', 'month', 'day')).issubset(kwargs):
-            postview = PostView.objects.find(kwargs)
-        else: raise Http404
-    except ObjectDoesNotExist:
-        raise Http404
+    if set(('slug', 'language', 'year', 'month', 'day')).issubset(kwargs):
+        postview = PostView.objects.find(kwargs)
+    else: raise Http404
     return render_to_response("posts/view.html", {'postview': postview}, RequestContext(request))
